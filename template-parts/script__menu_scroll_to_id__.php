402f7f57d9991ef1d76dee14ff8331bd1a02d09c
hitsuyo <nguyentansang3417@gmail.com>
2018-09-18 12:30:21 +0700

    Update

--- template-parts/script__menu_scroll_to_id__.php
+++ template-parts/script__menu_scroll_to_id__.php
@@ -3,6 +3,9 @@
 <script>
     jQuery(function(){
 
+        // menu
+        // Normal, only scroll to id in same page, error while opening other page
+        // _id or _class maybe need to modify 
         jQuery(".my-custom-main-menu-class ul.nav_main li.menu-item44 a").attr('id', 'btn-section-1'); //section 1, item 1
         jQuery(".my-custom-main-menu-class ul.nav_main li.menu-item26 a").attr('id', 'btn-section-2');
         jQuery(".my-custom-main-menu-class ul.nav_main li.menu-item38 a").attr('id', 'btn-section-3');
@@ -54,6 +57,48 @@
                 }, 1000);
         });
 
+        // jQuery(".my-custom-main-menu-class ul.nav_main li.menu-item44 a#btn-section-1").on("click",function(e) {
+        //         var jump=function(e)
+        //         {
+        //         if (e){
+        //         e.preventDefault();
+        //         var target = jQuery(this).attr("href");
+        //         }else{ var target = location.hash; }
+
+        //         jQuery('html,body').animate(
+        //         { scrollTop: jQuery("#section-1").offset().top },1000,function()
+        //                 {
+        //                 location.hash = target;
+        //                 });
+        //         }
+
+        //         jQuery('html, body').hide();
+
+        //         jQuery(document).ready(function()
+        //         {
+        //                 jQuery('a[href^=#]').bind("click", jump);
+
+        //         if (location.hash){
+        //                 setTimeout(function(){
+        //                 jQuery('html, body').scrollTop(0).show();
+        //                 jump();
+        //                 }, 0);
+        //         }else{
+        //                 jQuery('html, body').show();
+        //         }
+        //         });
+        // });
+
+    });
+
+</script>
+<script>
+jQuery(function(){
+
+        // Advanced, scroll to id anywhere, any page
+        // Menu with custom link
+        // Add href for Custom link: http://something/#section-1 (Admin page)
+        // section-1 : custom id, which wanna scroll to 
         jQuery(".my-custom-main-menu-class ul.nav_main li.menu-item44 a#btn-section-1").on("click",function(e) {
                 var jump=function(e)
                 {
@@ -86,7 +131,6 @@
                 });
         });
 
-    });
-
+});
 </script>
 <!-- Custom class into a-tag of main menu, to use scroll to id -->
